a103a1dbcafd98175721defdc169104ab0b7eb92
Houck <brooks.houck@accenture.com>
2022-05-24 15:58:39 -0400

    adding input variable

--- main.tf
+++ main.tf
@@ -4,5 +4,6 @@ module "networking" {
 
 module "ec2" {
     source = "./modules/ec2"
-    vpc = module.networking.vpc
+    vpc = module.networking.vpc.id
+    subnet_id = module.networking.vpc.public_subnets[0]
 }
