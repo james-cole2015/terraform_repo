8018ac58b04b431d4642df84ed4285cdeff6d8a7
james-cole2015 <brooks.houck@gmail.com>
2022-05-24 12:30:03 -0400

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -2,11 +2,11 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name                             = "${var.namespace}-vpc"
+  name                             = var.vpc_name
   cidr                             = var.cidr_block
   azs                              = data.aws_availability_zones.available.names
-  private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets                  = var.private_sn
+  public_subnets                   = var.public_sn
   #assign_generated_ipv6_cidr_block = true
   create_database_subnet_group     = true
   enable_nat_gateway               = true
