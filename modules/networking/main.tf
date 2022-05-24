7174ee1efeeaa94680c30da77b41385473b1bb68
Houck <brooks.houck@accenture.com>
2022-05-24 12:15:07 -0400

    add variables; added into network/main

---
+++ modules/networking/main.tf
@@ -0,0 +1,14 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name                             = "${var.namespace}-vpc"
+  cidr                             = var.cidr_block
+  azs                              = data.aws_availability_zones.available.names
+  private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
+  #assign_generated_ipv6_cidr_block = true
+  create_database_subnet_group     = true
+  enable_nat_gateway               = true
+  single_nat_gateway               = true
+}
---
+++ modules/networking/variables.tf
@@ -0,0 +1,14 @@
+variable cidr_block {
+    type = string
+    default = "10.100.0.0/16"
+}
+
+variable "private_sn" {
+    type = list(string)
+    default = [ "10.100.0.0","10.100.1.0" ]
+}
+
+variable "public_sn" {
+  type = list(string)
+  default = [ "10.100.32.0","10.100.42.0" ]
+}
