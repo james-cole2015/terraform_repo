1c79513ea6e88dcc7c77e5d08e9b2512ac6b3e45
james-cole2015 <brooks.houck@gmail.com>
2022-06-29 15:22:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "networking" {
 
 module "ec2" {
     source = "./modules/ec2"
-    vpc = module.networking.vpc.id
+    vpc = module.networking.vpc_id
     subnet_id = module.networking.vpc.public_subnets[0]
 }
