d399dbe9961f9c07bd61165e04eea50084a2f421
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-06-29 16:45:31 -0400

    created bootstrapping script

---
+++ ws_bootstrap.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo apt-get update \
+sudo apt-get install -y apache2 \
+git clone https://github.com/james-cole2015/wrecked-machine.git \
+cd wrecked-machine \
+cp -r * /var/www/html \
+sudo systemctl start apache2 \
+sudo systemctl enable apache2
