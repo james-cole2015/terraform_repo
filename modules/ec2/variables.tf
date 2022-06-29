207e5ab258b77d57e7316ce5ce7ffbdd4723258c
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-06-29 16:41:12 -0400

    added variable names for keypair

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -7,3 +7,8 @@ variable "subnet_id" {
   type = any
   default = ["$(module.networking.vpc.public_subnets[0]"]
 }
+
+variable "key_name" {
+  type = any
+  default = ["$module.aws_keys.key_pair.key_name"]
+}
