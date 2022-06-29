5042b2de9e773a292b076dd7c0feaff5478e6c20
james-cole2015 <brooks.houck@gmail.com>
2022-06-29 16:33:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,10 @@ module "ec2" {
     source = "./modules/ec2"
     vpc = module.networking.vpc_id
     subnet_id = module.networking.vpc.public_subnets[0]
+    key_name = module.key_gen.key_pair.key_name
 }
+
+module "key_gen" {
+    source = "./modules/aws_keys"
+}
+        
