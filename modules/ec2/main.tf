6e75255603721703e0329585663f621c1fd818f0
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-01 09:16:41 -0400

    modified instance name

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 4.0"
 
-  name = "single-instance"
+  name = "JacksFlamingRevenge"
 
   ami                    = "ami-0b0ea68c435eb488d"
   instance_type          = "t2.micro"
