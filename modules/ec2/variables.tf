ba3fd4e480d03aa3406f1db67800a63e63b71d6d
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-01 11:28:23 -0400

    adding sg to ec2 main

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -12,3 +12,8 @@ variable "key_name" {
   type = any
   default = ["$module.key_gen.key_pair_key_name"]
 }
+
+variable "security_group" {
+  type = any
+  default = ["module.networking.aws_security_group.allow_ssh.id"]
+}
