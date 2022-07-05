85106a5a43e7b18b186152c24b509ef927e20831
brooks <brooks.houck@gmail.com>
2022-07-05 22:12:06 +0000

    added sudo to cp command

--- ws_bootstrap.sh
+++ ws_bootstrap.sh
@@ -3,6 +3,6 @@ sudo apt-get update \
 sudo apt-get install -y apache2 \
 git clone https://github.com/james-cole2015/wrecked-machine.git \
 #cd wrecked-machine \
-cp -r * /var/www/html \
+sudo cp -r * /var/www/html \
 sudo systemctl start apache2 \
 sudo systemctl enable apache2
