33842d3e26d61f776dafa02ba20fe6e3ba5a5b58
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-05 18:36:31 -0400

    updated ingress rules

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -46,12 +46,12 @@ resource "aws_security_group" "allow_http" {
   description = "allow http traffic"
   vpc_id      = module.vpc.vpc_id
 
-  ingress = {
-    description = "http from internet"
+  ingress {
+    description = "http from the internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_block  = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
