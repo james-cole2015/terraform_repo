928f4f85d1a904f8b3f0fe9d0f4e0f28cad9aa65
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-05 20:47:41 -0400

    trying new user_data format

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -10,7 +10,10 @@ module "ec2_instance" {
   monitoring             = true
   vpc_security_group_ids = var.security_group
   subnet_id              = var.subnet_id
-  user_data              = file("ws_bootstrap.sh")
+  #user_data              = file("ws_bootstrap.sh")
+  user_data = <<EOF
+  git clone https://github.com/james-cole2015/wrecked-machine.git 
+  EOF 
 
   tags = {
     Terraform   = "true"
