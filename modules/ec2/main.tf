c709bbf127240374098905c3ed42cd97a3bd665c
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-05 21:28:50 -0400

    updating instance type & removing user_data comments

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,15 +5,13 @@ module "ec2_instance" {
   name = "JacksFlamingRevenge"
 
   ami                    = "ami-0439517b5e436bdab"
-  instance_type          = "t2.small"
+  instance_type          = "t2.micro"
   key_name               = "tf_key"
   monitoring             = true
   vpc_security_group_ids = var.security_group
   subnet_id              = var.subnet_id
-  #user_data              = file("ws_bootstrap.sh")
-  user_data = <<EOF
-  git clone https://github.com/james-cole2015/wrecked-machine.git 
-  EOF 
+  user_data              = file("ws_bootstrap.sh")
+
 
   tags = {
     Terraform   = "true"
