bc60c9c2b29c21a765be36c22f807862dd3a8764
brooks <brooks.houck@gmail.com>
2022-07-07 15:29:16 +0000

    removed public ip output

--- outputs.tf
+++ outputs.tf
@@ -13,8 +13,9 @@ output "public_sn0_id" {
 output "ec2_public_ip" {
   value = module.ec2.ec2_instance.public_ip
 }
-*/
+
 
 output "public_ip" {
   value = module.networking.vpc.aws_eip.nat[0]
 }
+*/
