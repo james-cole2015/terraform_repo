f994ea64a053541bb14c3ab4cb1736d5ade8d549
brooks <brooks.houck@gmail.com>
2022-07-07 15:26:41 +0000

    playing around with script

--- ws_bootstrap.sh
+++ ws_bootstrap.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 apt-get update &&
-apt-get install -y apache2 \
-git clone https://github.com/james-cole2015/wrecked-machine.git &&
-cd wrecked-machine &&
-cp -r * /var/www/html \
-systemctl start apache2 &&
-systemctl enable apache2
+apt-get install -y apache2 && 
+systemctl start apache2 
